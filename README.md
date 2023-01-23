f7100a068275ff1aa6497f83945227ef7ddc1027
Parking Lot Studio <76890242+ParkingLotGames@users.noreply.github.com>
2023-01-23 15:19:40 -0600

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# TS-React-JSON-Viewer
- Small demo React app to visualize JSON files with nested object support in Typescript.
+# Typescript React JSON Viewer
+
+![Website](https://img.shields.io/website?down_color=red&down_message=offline&style=plastic&up_color=green&up_message=online&url=https%3A%2F%2Ftypescript-react-json-viewer.vercel.app) ![License](https://img.shields.io/github/license/ParkingLotGames/TS-React-JSON-Viewer?style=plastic) ![Size](https://img.shields.io/github/repo-size/ParkingLotGames/TS-React-JSON-Viewer?style=plastic) ![package.json version (branch)](https://img.shields.io/github/package-json/v/ParkingLotGames/TS-React-JSON-Viewer/main?style=plastic) ![Last commit](https://img.shields.io/github/last-commit/ParkingLotGames/TS-React-JSON-Viewer?style=plastic)
+
+![Issues](https://img.shields.io/github/issues-raw/ParkingLotGames/TS-React-JSON-Viewer?style=plastic) ![Pull requests](https://img.shields.io/github/issues-pr-raw/ParkingLotGames/TS-React-JSON-Viewer?style=plastic)
+
+#### Small demo React app to visualize JSON files with nested object support in Typescript.
+- React 18
+- Vite 2.9.5
+- TailwindCSS 
+- PostCSS 
+- Autoprefixer
+- JSON-BigInt
+
+# Building
+
+### Prerequisites:
+
+- VSCode
+- Nodejs 16 LTS or higher
+
+### Build process:
+```
+npm install
+npm run build
+```
+### To run the app without building it, use:
+```
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import jsonBig from "json-bigint";
+
+const toTitleCase = (str: string) => {
+  // Replace any uppercase letter with a space and the letter
+  return str.replace(/([A-Z])/g, ' $1')
+    // Replace the first letter with its uppercase version
+    .replace(/^./, (str) => str.toUpperCase());
+};
+
+interface DisplayJSONDataProps {
+  data: object;
+}
+
+const DisplayJSONData: React.FC<DisplayJSONDataProps> = ({ data }) => {
+  // Helper function to check if the passed value is an object
+  const isObject = (obj: object) => obj === Object(obj);
+
+  return (
+    <div className="grid place-items-center">
+      {
+        // Iterate through all the keys of the data object
+        Object.keys(data).map((key: string) => (
+          <div className="w-[95vw] md:w-[512px]" key={key}>
+            {
+              // Check if the value of the key is a nested object (or array)
+              isObject(data[key]) ? (
+                <div>
+                  {/* If it's a nested object, display the key in bold */}
+<div className="text-lg font-bold w-full text-center bg-[#C0C0D0]">{toTitleCase(key)}</div>
+{/* Recursively call this component to handle nested objects  */}
+<DisplayJSONData data={data[key]} />
+</div>
+) : (
+<div className="text-lg text-gray-600">
+ {/* If it's not an object or array, display the key and value in small gray font  */}
+                  <div className="px-2 bg-gray-200 text-lg font-medium">{toTitleCase(key)}</div>
+                  <div className="px-2 bg-gray-100">{data[key]}</div>
+                </div>
+              )}
+          </div>
+        ))}
+    </div>
+  );
+};
+
+interface AppProps {
+}
+
+const App: React.FC<AppProps> = () => {
+  const [fileData, setFileData] = useState<object | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // Store the first file in the event's target files array in a variable
+    const file = event.target.files[0];
+    // Check if the file's type is "application/json"
+    if (file.type === "application/json") {
+      // Create a new FileReader object
+      const reader = new FileReader();
+      // Add an onload event listener to the reader
+      reader.onload = (e: any) => {
+        // Store the result of the read in a variable
+        const jsonString = e.target.result;
+        // Parse the json string and set it as FileData
+        setFileData(jsonBig.parse(jsonString));
+      };
+      // Start reading the file as text
+      reader.readAsText(file);
+    } else {
+      // If the file is not of type "application/json" show an alert
+      alert("Invalid file type, please select a JSON file.");
+    }
+  };
+
+  return (
+    <div className="container mx-auto py-4">
+      {/* Title of the JSON Viewer */}
+      <h1 className="text-2xl font-bold text-center mb-4">JSON Viewer</h1>
+      <div className="grid place-items-center mb-4">
+        {/* Input for selecting a file */}
+        <input
+          type="file"
+          onChange={handleFileChange}
+          className="p-2 w-[90vw] md:w-[384px] rounded-sm border border-gray-600"
+        />
+      </div>
+      {/* Only render the DisplayJSONData component if fileData is not null */}
+      {fileData && <DisplayJSONData data={fileData} />}
+    </div>
+  );
+}
+
+export default App;
